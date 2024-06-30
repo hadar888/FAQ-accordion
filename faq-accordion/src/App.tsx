3f5039ab0961a4c384770612570d579e0941dbfe
הדר שושן <hadarhadar888@gmail.com>
2024-06-30 21:30:51 +0300

    create api file

--- faq-accordion/src/App.tsx
+++ faq-accordion/src/App.tsx
@@ -1,36 +1,18 @@
-import { Center, Flex, MantineProvider, Paper, Text, Title, createTheme } from '@mantine/core';
+import { Center, Flex, MantineProvider, Paper, createTheme } from '@mantine/core';
 import { MantineEmotionProvider } from '@mantine/emotion';
 import '@mantine/core/styles.css';
 import React from 'react';
 import { colors } from './colors';
 import FaqAccordion from './FaqAccordion';
-import { Faq } from './modals/Faq';
 import { useMediaQuery } from '@mantine/hooks';
-
-const data: Faq[] = [
-  {
-    question:`What is Frontend Mentor, and how will it help me?`,
-    answer:`Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.`, 
-  },
-  {
-    question:`Is Frontend Mentor free?`,
-    answer:`Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.`,
-  },
-  {
-    question:`Can I use Frontend Mentor projects in my portfolio?`,
-    answer:`Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!`,
-  },
-  {
-    question:`How can I get help if I'm stuck on a Frontend Mentor challenge?`,
-    answer:`The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.`,
-  },
-];
+import { getFaqs } from './api/faqApi';
 
 const theme = createTheme({
   fontFamily: 'Work Sans',
 });
 
 function App() {
+  const data = getFaqs();
   const isMobile = useMediaQuery(`(max-width: 600px)`);
   
   return (
